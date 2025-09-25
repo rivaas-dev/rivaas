36946966c6aefb36d68a8ecf9ba3b5985394a90c
Olena Bushnieva <olena.bushnieva@company.info>
2025-09-25 09:26:59 +0000

    Resolve API-974 "Expose customers list"

--- internal/handler/v2/customers/list.go
+++ internal/handler/v2/customers/list.go
@@ -1,19 +1,26 @@
 package customers
 
 import (
+	"context"
 	"github.com/companyinfo/jsonapi"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 	"gitlab.ci.fdmg.org/ci-api/admin-api/internal"
 	"gitlab.ci.fdmg.org/ci-api/admin-api/internal/handler/v2/customers/filters"
 	"gitlab.ci.fdmg.org/ci-api/admin-api/internal/handler/v2/pagination"
+	"gitlab.ci.fdmg.org/ci-api/admin-api/internal/headers"
+	"gitlab.ci.fdmg.org/ci-api/go-pkgs/keycloak"
 	"gitlab.ci.fdmg.org/datacluster/golibs/goot"
 	"gitlab.ci.fdmg.org/datacluster/golibs/goskell"
+	"go.opentelemetry.io/otel/attribute"
 	"math"
 	"net/http"
 )
 
 // ListInput represents the request body of the LIST endpoint.
 type ListInput struct {
+	headers.Authorization
+
 	Match map[string]string
 	Query struct {
 		PaginationParams internal.PaginationParams
@@ -34,38 +41,19 @@ func (h *Handler) LIST(ctx *goskell.Context) {
 		return
 	}
 
-	searchParams, err := filters.NewSearchParameters(filters.FilterParam{Match: request.Match})
+	// Fetch keys from the database.
+	_, span := goot.Span(ctx.Request.Context(), "get_from_database")
+	customerGroups, totalResults, err := h.getCustomers(ctx.Request.Context(), request)
 	if err != nil {
-		goskell.JsonAPIError(ctx, "invalid query parameters", err, http.StatusBadRequest)
-		return
-	}
-
-	first := int((request.Query.PaginationParams.Page - 1) * request.Query.PaginationParams.Size)
-	max := int(request.Query.PaginationParams.Size)
-
-	// Fetch total group count
-	_, span := goot.Span(ctx.Request.Context(), "get_group_count_from_keycloak")
-	t := true
-	totalResults, err := h.keycloakClient.GetGroupsCount(ctx, searchParams.Name, &t)
-	if err != nil {
-		goot.EndSpanWithError(span, err, "failed to call keycloak")
-		goskell.JsonAPIError(ctx, http.StatusText(http.StatusInternalServerError), err, http.StatusInternalServerError)
-		return
-	}
-	goot.EndSpan(span)
-
-	// Fetch the requested page of the groups
-	_, span = goot.Span(ctx.Request.Context(), "get_from_keycloak")
-	groups, err := h.keycloakClient.GetGroupsPaginated(ctx, searchParams.Name, h.keycloakConfig.BrifRepresentation, first, max)
-	if err != nil {
-		goot.EndSpanWithError(span, err, "failed to call keycloak")
+		goot.EndSpanWithError(span, err, "failed to call database")
+		log.Err(err).Msg("error while communicating with DB")
 		goskell.JsonAPIError(ctx, http.StatusText(http.StatusInternalServerError), err, http.StatusInternalServerError)
 		return
 	}
 	goot.EndSpan(span)
 
 	// Parse the group data
-	parsedKeyCloakGroups := h.customerService.ParseCustomerGroups(groups)
+	parsedKeyCloakGroups := h.customerService.ParseCustomerGroups(customerGroups)
 
 	// Build response from groups
 	jsonAPIResponse, err := jsonapi.Marshal(parsedKeyCloakGroups)
@@ -104,6 +92,11 @@ func bindRequest(ctx *goskell.Context, defaultPageSize, maxPageSize uint) (*List
 		request ListInput
 		err     error
 	)
+	request.Authorization, err = headers.GetAuthorization(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	request.Query.PaginationParams, err = pagination.GetPagination(ctx, defaultPageSize, maxPageSize)
 	if err != nil {
 		log.Error().Err(err).Msg("invalid pagination parameters")
@@ -114,4 +107,48 @@ func bindRequest(ctx *goskell.Context, defaultPageSize, maxPageSize uint) (*List
 	//Filtering
 	request.Match = ctx.QueryMap("match")
 	return &request, nil
+}
+
+func (h *Handler) getCustomers(ctx context.Context, request *ListInput) (groups []*keycloak.Group, totalResults int, err error) {
+	// if the user is not an admin, return only their group
+	if !headers.IsAdministrator(request.Authorization.Roles) {
+		_, span := goot.Span(ctx, "get_from_keycloak", attribute.String("customer_id", request.Authorization.CustomerUser.CustomerID))
+		group, err := h.keycloakClient.GetGroupByID(ctx, request.Authorization.CustomerUser.CustomerID)
+		if err != nil {
+			goot.EndSpanWithError(span, err, "failed to call keycloak")
+			return nil, 0, errors.New("invalid customer id")
+		}
+		goot.EndSpan(span)
+
+		return []*keycloak.Group{group}, 1, nil
+	}
+
+	searchParams, err := filters.NewSearchParameters(filters.FilterParam{Match: request.Match})
+	if err != nil {
+		return nil, 0, err
+	}
+
+	firstElement := int((request.Query.PaginationParams.Page - 1) * request.Query.PaginationParams.Size)
+	maxPageSize := int(request.Query.PaginationParams.Size)
+
+	// Fetch total group count
+	_, span := goot.Span(ctx, "get_group_count_from_keycloak")
+	t := true
+	totalResults, err = h.keycloakClient.GetGroupsCount(ctx, searchParams.Name, &t)
+	if err != nil {
+		goot.EndSpanWithError(span, err, "failed to call keycloak")
+		return nil, 0, err
+	}
+	goot.EndSpan(span)
+
+	// Fetch the requested page of the groups
+	_, span = goot.Span(ctx, "get_from_keycloak")
+	groups, err = h.keycloakClient.GetGroupsPaginated(ctx, searchParams.Name, h.keycloakConfig.BrifRepresentation, firstElement, maxPageSize)
+	if err != nil {
+		goot.EndSpanWithError(span, err, "failed to call keycloak")
+		return nil, 0, err
+	}
+	goot.EndSpan(span)
+
+	return groups, totalResults, nil
 }
