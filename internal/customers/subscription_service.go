05ee68654eee86675a368dd84e89e34445331605
Mohammad Abdolirad <mohammad.abdolirad@company.info>
2025-11-18 08:45:17 +0100

    Update YAML tag for QuotaPolicyName in PricingPlan struct for improved configuration mapping

--- internal/customers/subscription_service.go
+++ internal/customers/subscription_service.go
@@ -35,7 +35,7 @@ type APIKey struct {
 
 // PricingPlan represents a pricing plan configuration
 type PricingPlan struct {
-	QuotaPolicyName           string `mapstructure:"quota"`
+	QuotaPolicyName           string `yaml:"quota" mapstructure:"quota"`
 	NumberOfAPIProductionKeys int    `mapstructure:"number_of_api_production_keys"`
 	NumberOfAPISandboxKeys    int    `mapstructure:"number_of_api_sandbox_keys"`
 }
