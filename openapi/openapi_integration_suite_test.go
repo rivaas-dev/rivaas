ebb82e1722e673e017d284dd99e0c11fd0c5cfec
Mohammad Abdolirad <m.abdolirad@gmail.com>
2026-01-16 15:28:00 +0100

    openapi: add build tags to separate unit and integration tests
    - add !integration tag to unit test files in openapi
    - add integration tag to integration test files
    - remove redundant testing.Short() skip check

--- openapi/openapi_integration_suite_test.go
+++ openapi/openapi_integration_suite_test.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build integration
+
 package openapi_test
 
 import (
@@ -23,9 +25,6 @@ import (
 
 //nolint:paralleltest // Ginkgo test suite manages its own parallelization
 func TestOpenAPIIntegration(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping integration test in short mode")
-	}
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "OpenAPI Integration Suite")
 }
